e0b10b896c2f4831a41bf964690190f06575a654
hande <leader22822632@gmail.com>
2025-05-22 03:56:12 +0800

    Implement coroutine in xo-user.c
    Implemented coroutine framework with multiple tasks including keyboard event handling,
    AI move for 'X' and 'O', and board rendering.
    Added task scheduling and switching logic to enable cooperative multitasking among these tasks.

--- Makefile
+++ Makefile
@@ -1,19 +1,34 @@
-TARGET = kxo
-kxo-objs = main.o game.o xoroshiro.o mcts.o negamax.o zobrist.o
-obj-m := $(TARGET).o
+TARGET      = kxo
+kxo-objs    = main.o game.o xoroshiro.o mcts.o negamax.o zobrist.o
+obj-m      := $(TARGET).o
 
-ccflags-y := -std=gnu99 -Wno-declaration-after-statement
-KDIR ?= /lib/modules/$(shell uname -r)/build
-PWD := $(shell pwd)
+ccflags-y   := -std=gnu99 -Wno-declaration-after-statement
+KDIR       ?= /lib/modules/$(shell uname -r)/build
+PWD         := $(shell pwd)
+
+
+USER_SRC  := xo-user.c \
+             user_game.c user_mcts.c user_negamax.c \
+             user_xoroshiro.c user_zobrist.c
+USER_OBJ  := $(USER_SRC:.c=.o)
+
+USER_CFLAGS := -std=gnu99 -Wno-declaration-after-statement -Wall -g
+USER_LDFLAGS := 
 
 GIT_HOOKS := .git/hooks/applied
 all: kmod xo-user
 
+
 kmod: $(GIT_HOOKS) main.c
 	$(MAKE) -C $(KDIR) M=$(PWD) modules
 
-xo-user: xo-user.c
-	$(CC) $(ccflags-y) -o $@ $<
+xo-user: $(USER_OBJ)
+	$(CC) $(USER_CFLAGS) $(USER_LDFLAGS) -o $@ $^
+
+
+%.o: %.c
+	$(CC) $(USER_CFLAGS) -c $< -o $@
+
 
 $(GIT_HOOKS):
 	@scripts/install-git-hooks
@@ -22,4 +37,4 @@ $(GIT_HOOKS):
 
 clean:
 	$(MAKE) -C $(KDIR) M=$(PWD) clean
-	$(RM) xo-user
+	$(RM) xo-user $(USER_OBJ)
--- xo-user.c
+++ xo-user.c
@@ -1,6 +1,8 @@
 #include <fcntl.h>
 #include <getopt.h>
+#include <setjmp.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,32 +10,179 @@
 #include <termios.h>
 #include <time.h>
 #include <unistd.h>
+#include "list.h"
+#include "user_game.h"
+#include "user_mcts.h"
+#include "user_negamax.h"
 
-#include "game.h"
+// #include "game.h"
 
 #define XO_STATUS_FILE "/sys/module/kxo/initstate"
 #define XO_DEVICE_FILE "/dev/kxo"
 #define XO_DEVICE_ATTR_FILE "/sys/class/kxo/kxo/kxo_state"
 
-static bool status_check(void)
+struct task {
+    jmp_buf env;
+    struct list_head list;
+};
+
+struct user_attr {
+    int display;
+    int end;
+};
+
+static struct user_attr attr_obj;
+static char board[N_GRIDS];
+static LIST_HEAD(tasklist);
+static jmp_buf sched;
+static void (**tasks)(void);
+static int ntasks;
+static char display_buf[DRAWBUFFER_SIZE];
+static struct task *cur_task;
+
+static void task_add(struct task *task)
 {
-    FILE *fp = fopen(XO_STATUS_FILE, "r");
-    if (!fp) {
-        printf("kxo status : not loaded\n");
-        return false;
+    list_add_tail(&task->list, &tasklist);
+}
+
+static void task_switch()
+{
+    if (!list_empty(&tasklist)) {
+        struct task *t = list_first_entry(&tasklist, struct task, list);
+        list_del(&t->list);
+        cur_task = t;
+        longjmp(t->env, 1);
+    }
+}
+void schedule(void)
+{
+    static int i;
+
+    setjmp(sched);
+
+    while (ntasks-- > 0) {
+        tasks[i++]();
+        printf("Never reached\n");
     }
 
-    char read_buf[20];
-    fgets(read_buf, 20, fp);
-    read_buf[strcspn(read_buf, "\n")] = 0;
-    if (strcmp("live", read_buf)) {
-        printf("kxo status : %s\n", read_buf);
-        fclose(fp);
-        return false;
-    }
-    fclose(fp);
-    return true;
+    task_switch();
 }
+
+void task_O(void)
+{
+    struct task *task = malloc(sizeof(struct task));
+    INIT_LIST_HEAD(&task->list);
+
+    if (setjmp(task->env) == 0) {
+        task_add(task);
+        longjmp(sched, 1);
+    }
+
+    task = cur_task;
+
+    while (!attr_obj.end) {
+        if (setjmp(task->env) == 0) {
+            int move;
+            move = mcts(board, 'O');
+            if (move != -1)
+                board[move] = 'O';
+            task_add(task);
+            task_switch();
+        }
+        task = cur_task;
+        char win = check_win(board);
+        if (win != ' ') {
+            memset(board, ' ', N_GRIDS);
+        }
+    }
+    free(task);
+    longjmp(sched, 1);
+}
+
+void task_X(void)
+{
+    struct task *task = malloc(sizeof(struct task));
+    INIT_LIST_HEAD(&task->list);
+
+    if (setjmp(task->env) == 0) {
+        task_add(task);
+        longjmp(sched, 1);
+    }
+
+    task = cur_task;
+
+    while (!attr_obj.end) {
+        if (setjmp(task->env) == 0) {
+            int move;
+            move = negamax_predict(board, 'X').move;
+            if (move != -1) {
+                board[move] = 'X';
+            }
+            task_add(task);
+            task_switch();
+        }
+        task = cur_task;
+        char win = check_win(board);
+        if (win != ' ') {
+            memset(board, ' ', N_GRIDS);
+        }
+    }
+    free(task);
+    longjmp(sched, 1);
+}
+
+void task_draw(void)
+{
+    struct task *task = malloc(sizeof(struct task));
+    INIT_LIST_HEAD(&task->list);
+
+    if (setjmp(task->env) == 0) {
+        task_add(task);
+        longjmp(sched, 1);
+    }
+
+    task = cur_task;
+
+    while (!attr_obj.end) {
+        if (setjmp(task->env) == 0) {
+            if (!attr_obj.display) {
+                task_add(task);
+                task_switch();
+            }
+
+            task = cur_task;
+
+            int i = 0, k = 0;
+            display_buf[i++] = '\n';
+            display_buf[i++] = '\n';
+
+            while (i < DRAWBUFFER_SIZE) {
+                for (int j = 0; j < (BOARD_SIZE << 1) - 1 && k < N_GRIDS; j++) {
+                    display_buf[i++] = j & 1 ? '|' : board[k++];
+                }
+                display_buf[i++] = '\n';
+                for (int j = 0; j < (BOARD_SIZE << 1) - 1; j++) {
+                    display_buf[i++] = '-';
+                }
+                display_buf[i++] = '\n';
+            }
+            time_t now = time(NULL);
+            const struct tm *t = localtime(&now);
+            printf("\033[H\033[J");
+            display_buf[DRAWBUFFER_SIZE - 1] = '\0';
+            printf("%s", display_buf);
+            printf("\n%04d-%02d-%02d %02d:%02d:%02d\n", t->tm_year + 1900,
+                   t->tm_mon + 1, t->tm_mday, t->tm_hour, t->tm_min, t->tm_sec);
+
+            task_add(task);
+            task_switch();
+        }
+        task = cur_task;
+    }
+    free(task);
+    longjmp(sched, 1);
+}
+
 
 static struct termios orig_termios;
 
@@ -52,122 +201,226 @@ static void raw_mode_enable(void)
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
-static bool read_attr, end_attr;
 
-static void listen_keyboard_handler(void)
+static void task_keyboard_handler(void)
 {
-    int attr_fd = open(XO_DEVICE_ATTR_FILE, O_RDWR);
+    struct task *task = malloc(sizeof(struct task));
+    INIT_LIST_HEAD(&task->list);
+
+    if (setjmp(task->env) == 0) {
+        task_add(task);
+        longjmp(sched, 1);
+    }
+
+    task = cur_task;
+
     char input;
-
-    if (read(STDIN_FILENO, &input, 1) == 1) {
-        char buf[20];
-        switch (input) {
-        case 16: /* Ctrl-P */
-            read(attr_fd, buf, 6);
-            buf[0] = (buf[0] - '0') ? '0' : '1';
-            read_attr ^= 1;
-            write(attr_fd, buf, 6);
-            if (!read_attr)
-                printf("\n\nStopping to display the chess board...\n");
-            break;
-        case 17: /* Ctrl-Q */
-            read(attr_fd, buf, 6);
-            buf[4] = '1';
-            read_attr = false;
-            end_attr = true;
-            write(attr_fd, buf, 6);
-            printf("\n\nStopping the kernel space tic-tac-toe game...\n");
-            break;
+    while (!attr_obj.end) {
+        if (setjmp(task->env) == 0) {
+            if (read(STDIN_FILENO, &input, 1) == 1) {
+                switch (input) {
+                case 16: /*CTRL P*/
+                    attr_obj.display ^= 1;
+                    if (!attr_obj.display)
+                        printf("Stopping to display the chess board...\n");
+                    break;
+                case 17: /*CTRL Q*/
+                    attr_obj.end = 1;
+                    printf("\n\nStopping the tic-tac-toe game...\n");
+                    break;
+                }
+            }
+            task_add(task);
+            task_switch();
         }
+        task = cur_task;
     }
-    close(attr_fd);
+    free(task);
+    longjmp(sched, 1);
 }
 
-static void decoding(unsigned int decoding_val, char *display_buf)
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+
+int main()
 {
-    char table[N_GRIDS];
-    memset(table, ' ', sizeof(table));
-    for (int i = N_GRIDS - 1; i >= 0; i--) {
-        unsigned int val = (decoding_val >> (i << 1)) & 0x3;
-        switch (val) {
-        case 0x0:
-            table[N_GRIDS - 1 - i] = ' ';
-            break;
-        case 0x2:
-            table[N_GRIDS - 1 - i] = 'X';
-            break;
-        case 0x3:
-            table[N_GRIDS - 1 - i] = 'O';
-            break;
-        }
-    }
-
-    int i = 0, k = 0;
-    display_buf[i++] = '\n';
-    display_buf[i++] = '\n';
-
-    while (i < DRAWBUFFER_SIZE) {
-        for (int j = 0; j < (BOARD_SIZE << 1) - 1 && k < N_GRIDS; j++) {
-            display_buf[i++] = j & 1 ? '|' : table[k++];
-        }
-        display_buf[i++] = '\n';
-        for (int j = 0; j < (BOARD_SIZE << 1) - 1; j++) {
-            display_buf[i++] = '-';
-        }
-        display_buf[i++] = '\n';
-    }
-}
-
-int main(int argc, char *argv[])
-{
-    if (!status_check())
-        exit(1);
-
     raw_mode_enable();
     int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
     fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
+    attr_obj.display = 1;
+    attr_obj.end = 0;
 
-    static char display_buf[DRAWBUFFER_SIZE] = {0};
-    unsigned int decoding_val __attribute__((aligned(64)));
+    mcts_init();
+    negamax_init();
+    INIT_LIST_HEAD(&tasklist);
 
-    fd_set readset;
-    int device_fd = open(XO_DEVICE_FILE, O_RDONLY);
-    int max_fd = device_fd > STDIN_FILENO ? device_fd : STDIN_FILENO;
-    read_attr = true;
-    end_attr = false;
-
-    while (!end_attr) {
-        FD_ZERO(&readset);
-        FD_SET(STDIN_FILENO, &readset);
-        FD_SET(device_fd, &readset);
-
-        int result = select(max_fd + 1, &readset, NULL, NULL, NULL);
-        if (result < 0) {
-            printf("Error with select system call\n");
-            exit(1);
-        }
-
-        if (FD_ISSET(STDIN_FILENO, &readset)) {
-            FD_CLR(STDIN_FILENO, &readset);
-            listen_keyboard_handler();
-        } else if (read_attr && FD_ISSET(device_fd, &readset)) {
-            time_t now = time(NULL);
-            const struct tm *t = localtime(&now);
-            FD_CLR(device_fd, &readset);
-            printf("\033[H\033[J"); /* ASCII escape code to clear the screen */
-            read(device_fd, &decoding_val, sizeof(decoding_val));
-            decoding(decoding_val, display_buf);
-            display_buf[DRAWBUFFER_SIZE - 1] = '\0';
-            printf("%s", display_buf);
-            printf("\n%04d-%02d-%02d %02d:%02d:%02d\n", t->tm_year + 1900,
-                   t->tm_mon + 1, t->tm_mday, t->tm_hour, t->tm_min, t->tm_sec);
-        }
-    }
+    void (*registered_tasks[])(void) = {
+        task_O, task_keyboard_handler, task_draw, task_keyboard_handler,
+        task_X, task_keyboard_handler, task_draw, task_keyboard_handler};
+    tasks = registered_tasks;
+    ntasks = ARRAY_SIZE(registered_tasks);
+    schedule();
 
     raw_mode_disable();
     fcntl(STDIN_FILENO, F_SETFL, flags);
 
-    close(device_fd);
-
     return 0;
 }
+// static bool status_check(void)
+// {
+//     FILE *fp = fopen(XO_STATUS_FILE, "r");
+//     if (!fp) {
+//         printf("kxo status : not loaded\n");
+//         return false;
+//     }
+
+//     char read_buf[20];
+//     fgets(read_buf, 20, fp);
+//     read_buf[strcspn(read_buf, "\n")] = 0;
+//     if (strcmp("live", read_buf)) {
+//         printf("kxo status : %s\n", read_buf);
+//         fclose(fp);
+//         return false;
+//     }
+//     fclose(fp);
+//     return true;
+// }
+
+
+
+// static struct termios orig_termios;
+
+// static void raw_mode_disable(void)
+// {
+//     tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+// }
+
+// static void raw_mode_enable(void)
+// {
+//     tcgetattr(STDIN_FILENO, &orig_termios);
+//     atexit(raw_mode_disable);
+//     struct termios raw = orig_termios;
+//     raw.c_iflag &= ~IXON;
+//     raw.c_lflag &= ~(ECHO | ICANON);
+//     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+// }
+
+// static bool read_attr, end_attr;
+
+// static void listen_keyboard_handler(void)
+// {
+//     int attr_fd = open(XO_DEVICE_ATTR_FILE, O_RDWR);
+//     char input;
+
+//     if (read(STDIN_FILENO, &input, 1) == 1) {
+//         char buf[20];
+//         switch (input) {
+//         case 16: /* Ctrl-P */
+//             read(attr_fd, buf, 6);
+//             buf[0] = (buf[0] - '0') ? '0' : '1';
+//             read_attr ^= 1;
+//             write(attr_fd, buf, 6);
+//             if (!read_attr)
+//                 printf("\n\nStopping to display the chess board...\n");
+//             break;
+//         case 17: /* Ctrl-Q */
+//             read(attr_fd, buf, 6);
+//             buf[4] = '1';
+//             read_attr = false;
+//             end_attr = true;
+//             write(attr_fd, buf, 6);
+//             printf("\n\nStopping the kernel space tic-tac-toe game...\n");
+//             break;
+//         }
+//     }
+//     close(attr_fd);
+// }
+
+// static void decoding(unsigned int decoding_val, char *display_buf)
+// {
+//     char table[N_GRIDS];
+//     memset(table, ' ', sizeof(table));
+//     for (int i = N_GRIDS - 1; i >= 0; i--) {
+//         unsigned int val = (decoding_val >> (i << 1)) & 0x3;
+//         switch (val) {
+//         case 0x0:
+//             table[N_GRIDS - 1 - i] = ' ';
+//             break;
+//         case 0x2:
+//             table[N_GRIDS - 1 - i] = 'X';
+//             break;
+//         case 0x3:
+//             table[N_GRIDS - 1 - i] = 'O';
+//             break;
+//         }
+//     }
+
+//     int i = 0, k = 0;
+//     display_buf[i++] = '\n';
+//     display_buf[i++] = '\n';
+
+//     while (i < DRAWBUFFER_SIZE) {
+//         for (int j = 0; j < (BOARD_SIZE << 1) - 1 && k < N_GRIDS; j++) {
+//             display_buf[i++] = j & 1 ? '|' : table[k++];
+//         }
+//         display_buf[i++] = '\n';
+//         for (int j = 0; j < (BOARD_SIZE << 1) - 1; j++) {
+//             display_buf[i++] = '-';
+//         }
+//         display_buf[i++] = '\n';
+//     }
+// }
+
+// int main(int argc, char *argv[])
+// {
+//     if (!status_check())
+//         exit(1);
+
+//     raw_mode_enable();
+//     int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
+//     fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
+
+//     static char display_buf[DRAWBUFFER_SIZE] = {0};
+//     unsigned int decoding_val __attribute__((aligned(64)));
+
+//     fd_set readset;
+//     int device_fd = open(XO_DEVICE_FILE, O_RDONLY);
+//     int max_fd = device_fd > STDIN_FILENO ? device_fd : STDIN_FILENO;
+//     read_attr = true;
+//     end_attr = false;
+
+//     while (!end_attr) {
+//         FD_ZERO(&readset);
+//         FD_SET(STDIN_FILENO, &readset);
+//         FD_SET(device_fd, &readset);
+
+//         int result = select(max_fd + 1, &readset, NULL, NULL, NULL);
+//         if (result < 0) {
+//             printf("Error with select system call\n");
+//             exit(1);
+//         }
+
+//         if (FD_ISSET(STDIN_FILENO, &readset)) {
+//             FD_CLR(STDIN_FILENO, &readset);
+//             listen_keyboard_handler();
+//         } else if (read_attr && FD_ISSET(device_fd, &readset)) {
+//             time_t now = time(NULL);
+//             const struct tm *t = localtime(&now);
+//             FD_CLR(device_fd, &readset);
+//             printf("\033[H\033[J"); /* ASCII escape code to clear the screen
+//             */ read(device_fd, &decoding_val, sizeof(decoding_val));
+//             decoding(decoding_val, display_buf);
+//             display_buf[DRAWBUFFER_SIZE - 1] = '\0';
+//             printf("%s", display_buf);
+//             printf("\n%04d-%02d-%02d %02d:%02d:%02d\n", t->tm_year + 1900,
+//                    t->tm_mon + 1, t->tm_mday, t->tm_hour, t->tm_min,
+//                    t->tm_sec);
+//         }
+//     }
+
+//     raw_mode_disable();
+//     fcntl(STDIN_FILENO, F_SETFL, flags);
+
+//     close(device_fd);
+
+//     return 0;
+// }
